ea8fe9f096b4fb53d2ec36266219bdade2060597
Mike <mwahba@ucalgary.ca>
2021-02-21 09:59:58 -0700

    Exhaustion WIP

---
+++ Assets/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward);
+        
+        if(Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left);
+        
+        if(Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right);
+        
+        if(Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back);
+        
+        if(Input.GetKey(KeyCode.LeftControl))
+            transform.Translate(Vector3.down);
+        
+        if(Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up);
+        
+        if(Input.GetKey(KeyCode.E))
+            transform.Rotate(Vector3.up,50.0f*Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.Q))
+            transform.Rotate(Vector3.up,-50.0f*Time.deltaTime);
+        
+    }
+}
